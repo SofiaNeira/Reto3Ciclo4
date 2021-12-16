351513a40eb3e6b70294b4a9e1efb3a7cce0c3b3
sofíaN <91507303+SofiaNeira@users.noreply.github.com>
2021-12-16 07:56:39 -0500

    first commit

---
+++ src/main/java/com/exampleG8/Reto3Ciclo4/Reto3Ciclo4Application.java
@@ -0,0 +1,13 @@
+package com.exampleG8.Reto3Ciclo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto3Ciclo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto3Ciclo4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/exampleG8/Reto3Ciclo4/Reto3Ciclo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleG8.Reto3Ciclo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto3Ciclo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
